4feaed857e2d367e697633496787d1cfdf30d050
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-05-21 13:17:57 +0530

    hero section completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <h1>a community made of, by</h1>
+              <h1>and for students.</h1>
+            </div>
+            <div>
+              <p>get in touch</p>
+              <div>
+                <input type="email" name="" id="" />
+                <button>done</button>
+              </div>
+            </div>
+            <div>
+              <p>contact us</p>
+              <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+              </div>
+            </div>
+            <div>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,28 +1,55 @@
-import React from 'react'
-import { NavBar } from './NavBar'
+import React from "react";
+import { NavBar } from "./NavBar";
 import { GrFormNextLink } from "react-icons/gr";
+import mainBg from "../assets/mainBg.png";
 
 const LandingPage = () => {
   return (
-    <div className='lg:h-[1120px] bg-cover  filter-blue blur-3x text-white'>
-          <NavBar/>
-          <div className='mt-[300px] '>
-             <h1 className='lg:text-[80px] text-[60px] font-bold '>unlock your potential with <br/>
-             our community.
-             </h1>
-
-             <h1 className='lg:text-[25px] text-[18px] mt-8'>connect,learn,achieve,and earn with our community. </h1>
+    <>
+      <div
+        style={{ backgroundImage: `url(${mainBg})` }}
+        className="bg-cover bg-center"
+      >
+        <header>
+          <nav>
+            <div>
+              <div>
+                <img src="" alt="" />
+              </div>
+              <div>
+                <button>login</button>
+                <button>get started</button>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <section className="flex select-none items-center justify-center min-h-screen h-full relative">
+          <div className="flex items-center justify-center mx-auto p-6 sm:p-12 lg:p-16">
+            <div className="flex flex-col items-center justify-center lg:gap-12 md:gap-10 gap-8">
+              <div className="flex flex-col justify-center items-center text-center text-white md:w-auto w-[90%] mx-auto gap-4">
+                <div className="flex flex-col justify-center items-center text-center gap-3">
+                  <h1 className="lg:text-6xl md:text-4xl text-2xl font-bold leading-7">
+                    unlock your potential with
+                  </h1>
+                  <h1 className="lg:text-6xl md:text-4xl text-2xl font-bold leading-7">
+                    our community.
+                  </h1>
+                </div>
+                <p className="lg:text-2xl md:text-xl text-lg font-semibold">
+                  connect, learn, archieve, and earn with our community.
+                </p>
+              </div>
+              <div className="flex flex-col justify-center items-center w-full">
+                <button className="uppercase text-[#6921EF] font-bold lg:text-2xl md:text-xl text-lg text-center bg-white px-7 py-3 rounded-[12px] shadow-md hover:shadow-xl transition-all duration-300 ease-in-out ">
+                  join us
+                </button>
+              </div>
+            </div>
           </div>
-<div className='flex justify-center mt-24'>
-<button className="text-base font-bold text-purple-600 bg-white  w-52 flex items-center justify-center rounded-2xl h-12">
-              Get Started <GrFormNextLink size={20} />
-            </button>
-</div>
+        </section>
+      </div>
+    </>
+  );
+};
 
-
-          
-    </div>
-  )
-}
-
-export default LandingPage
+export default LandingPage;
